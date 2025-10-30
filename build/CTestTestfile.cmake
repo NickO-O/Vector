bf7c3c0e5a9bc63ad6a0c1bb8cbf4d5d1c8d8000
Orlov Nick <Andrefrolov34@yandex.ru>
2025-10-30 23:42:59 +0300

    add a lot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/user/Desktop/Vector
+# Build directory: /mnt/c/Users/user/Desktop/Vector/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(vector_tests "/mnt/c/Users/user/Desktop/Vector/build/vector_tests")
+set_tests_properties(vector_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/user/Desktop/Vector/CMakeLists.txt;19;add_test;/mnt/c/Users/user/Desktop/Vector/CMakeLists.txt;0;")
