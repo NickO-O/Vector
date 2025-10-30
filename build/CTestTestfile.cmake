c36c78887686197509616fd781f7d8d91cbaad12
Orlov Nick <Andrefrolov34@yandex.ru>
2025-10-31 00:38:11 +0300

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/user/Desktop/Vector
-# Build directory: /mnt/c/Users/user/Desktop/Vector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(vector_tests "/mnt/c/Users/user/Desktop/Vector/build/vector_tests")
-set_tests_properties(vector_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/user/Desktop/Vector/CMakeLists.txt;18;add_test;/mnt/c/Users/user/Desktop/Vector/CMakeLists.txt;0;")
